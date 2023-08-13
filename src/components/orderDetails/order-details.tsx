58e954e5f7dc5c10318a959e83bf6d6c234a48c0
Ravil Shamilov <75186385+Ravil377@users.noreply.github.com>
2023-08-13 14:52:33 -0600

    fix order

--- src/components/orderDetails/order-details.tsx
+++ src/components/orderDetails/order-details.tsx
@@ -1,17 +1,15 @@
 import orderDetailsStyles from './order-details.module.css';
 import { start, wait } from '../../utils/constants';
 import success from "../../images/success.svg";
-import { useSelector } from 'react-redux';
+import { useAppSelector } from '../../utils/chema';
 
 export const OrderDetails = () => {
-    // @ts-ignore
-    const { data } = useSelector(state => ({ data: state.order.order }));  
-    const { name, order } = data;
+    const { order } = useAppSelector(state => ({ order: state.order }));  
     
     return (
         <div className={orderDetailsStyles.container}>
-            <p className={`${orderDetailsStyles.order} text text_type_digits-large mb-8`}>{order.number}</p>
-            <p className={`${orderDetailsStyles.title} text text_type_main-medium mb-15`}>{name}</p>
+            <p className={`${orderDetailsStyles.order} text text_type_digits-large mb-8`}>{order?.order?.order.number}</p>
+            <p className={`${orderDetailsStyles.title} text text_type_main-medium mb-15`}>{order?.order?.name}</p>
             <img className={`${orderDetailsStyles.success}`} src={success} />
             <p className={`text text_type_main-default mt-15 ${orderDetailsStyles.start}`}>{start}</p>
             <p className={`text text_type_main-default text_color_inactive mt-2 ${orderDetailsStyles.wait}`}>{wait}</p>
